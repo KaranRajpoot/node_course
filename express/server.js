59fe2800f2b521f1621bb7aed6814237b4c0e0a9
Karan Singh <karanrajpoot@karans-MacBook-Pro.local>
2018-04-07 15:52:34 +0530

    bug fixed

--- express/server.js
+++ express/server.js
@@ -1,38 +1,40 @@
 var express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
-app.use(express.static(__dirname +"/public"));
-hbs.registerPartials(__dirname +"/views/partial");
-hbs.registerHelper('getRegisterYear',()=>{
+var  partialPath = __dirname +"/views";
+console.log(` Path: ${partialPath}`);
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('Engine View','hbs');
+hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear();
 });
 hbs.registerHelper('upperCased',(text)=>{
   return text.toUpperCase();
-})
+});
 app.use(function (req,res,next){
   var now = new Date().toString();
   console.log(now + "\nMethod:"+req.method + "\nURL:"+req.url);
   next();
 });
-app.set('Engine View','hbs');
 app.get('/home',(req,res)=>{
-res.render('home.hbs',{title:'Welcome to my Website',description:'this is a home page of my site'});
+res.render('home.hbs',{pageTitle:'Welcome to my Website',welcomeMessage:'Welcome to my First Node JS Application',description:'this is a home page of my site'});
 });
-
 app.get('/help',(req,res)=>{
-res.render('help.hbs',{title:'About help page',description:'help desc'});
+res.render('help.hbs',{pageTitle:'About help page',description:'help desc'});
 });
-
 
 app.get('/',(req,res)=>{
-res.send({
-  name:'karan',
-  likes:['biking','coding']
-});
-});
+// res.send({
+//   name:'karan',
+//   likes:['biking','coding']
+// });
+res.render('home.hbs',{pageTitle:'Welcome to my Website',welcomeMessage:'Welcome to my First Node JS Application',description:'this is a home page of my site'});
 
-
+});
 app.listen(port,()=>{
  console.log(`server is up at port ${port}`);
 });
